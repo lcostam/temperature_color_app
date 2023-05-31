4bec5340f7b5d0e5e1abf13557119e3307d24021
Leandro Miranda <lcostamiranda@gmail.com>
2023-05-30 22:54:37 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [temperature, setTemperature] = useState(25);
+  const [tempColor, setTempColor] = useState('warm');
+
+  const decrementTemp = () => {
+   const newTemp = temperature-1;
+   updateColor(newTemp);
+   setTemperature(newTemp);
+   
+  };
+
+  const incrementTemp = () => {
+    const newTemp = temperature+1;
+    updateColor(newTemp);
+    setTemperature(newTemp);
+   };
+
+
+   const updateColor = (newTemp) => {
+    if(newTemp>=30){
+      setTempColor('hot')
+     }else if(newTemp<=15){
+      setTempColor('cold')
+     }else{
+      setTempColor('warm')
+     }
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <div className={`circle ${tempColor}`}>
+      <span className="temperature">{temperature}</span>
     </div>
+    <div className="buttons">
+      <button className={`material-icons ${tempColor}`} onClick={decrementTemp}>remove</button>
+      <button className={`material-icons ${tempColor}`} onClick={incrementTemp}>add</button>
+    </div>
+  </div>
   );
 }
 
